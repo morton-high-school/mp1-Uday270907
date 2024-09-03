01266c8814f1e8205239a1a952402d8cff29888f
Uday270907 <uday.anand@mcusd709.org>
2024-09-03 09:06:38 -0500

    Fixed the stuff I did wrong

---
+++ AboutMe.java
@@ -0,0 +1,8 @@
+public class AboutMe{
+    public static void main(String[] args){
+        System.out.println("My name is Uday Anand.");
+        System.out.println("I am a Junior at Morton High School, which is located in Morton, IL.");
+        System.out.println("I live in the Orchard Hills subdivision here in Morton, which put me in Lincoln Elementary School when I was younger.");
+        System.out.print("Mr. Zeller is one of my favorite teachers at the school because his attitude is what I would describe as 'chill'.");
+    }
+}
